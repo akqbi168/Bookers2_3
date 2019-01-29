289ccce4c7f7b3391c31a732e97e2315f1a7b570
akqbi168 <akqbi168@gmail.com>
2019-01-29 09:47:18 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show]
+  root 'home#top'
+  get '/about' => 'home#about'
+
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
